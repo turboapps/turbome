6e405d1fdc3098a6c254b660e851494037c66833
olegsomphane <oleg@somphane.com>
2014-08-20 12:30:48 -0700

    initPHP update
    needs & ver > nul hack as well

--- wordpress/initPHP.cmd
+++ wordpress/initPHP.cmd
@@ -1,4 +1,4 @@
 copy c:\php\php.ini-production c:\php\php.ini
-%spn_fart% -C c:\php\php.ini "; extension_dir = \"./\"" "extension_dir = \"./\""
-%spn_fart% -C c:\php\php.ini "; extension_dir = \"ext\"" "extension_dir = \"ext\""
-%spn_fart% c:\php\php.ini ";extension=php_mysql.dll" "extension=php_mysql.dll"
+%spn_fart% -C c:\php\php.ini "; extension_dir = \"./\"" "extension_dir = \"./\"" & ver > nul
+%spn_fart% -C c:\php\php.ini "; extension_dir = \"ext\"" "extension_dir = \"ext\""  & ver > nul
+%spn_fart% c:\php\php.ini ";extension=php_mysql.dll" "extension=php_mysql.dll"  & ver > nul
