554927775354bc3afa8e66fa5b2660a20a7103b0
olegsomphane <oleg@somphane.com>
2014-08-20 12:57:33 -0700

    retry db creation if fails
    may fail if the process doesn't start fast enough

--- wordpress/initMySQL.cmd
+++ wordpress/initMySQL.cmd
@@ -2,6 +2,8 @@ start "mysql" mysqld.exe
 
 echo CREATE DATABASE wordpress;> spn_cmd.txt
 
+:createDB
 mysql -u root < spn_cmd.txt
+IF %ERRORLEVEL% NEQ 0 GOTO createDB
 
 mysqladmin.exe -u root shutdown
