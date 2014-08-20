4c61827a05ee3290ad41aa1cb2da1ddcceb94162
olegsomphane <oleg@somphane.com>
2014-08-20 12:23:47 -0700

    wordpress script updates
    fixed initPHP (have to replace lines instead of adding new ones; order
    matters)
    fixed wordpress spoon.me file to auto-config the db connection

--- wordpress/initPHP.cmd
+++ wordpress/initPHP.cmd
@@ -1,3 +1,4 @@
 copy c:\php\php.ini-production c:\php\php.ini
-echo extension_dir = "ext">> c:\php\php.ini
-echo extension=php_mysql.dll>> c:\php\php.ini
+%spn_fart% -C c:\php\php.ini "; extension_dir = \"./\"" "extension_dir = \"./\""
+%spn_fart% -C c:\php\php.ini "; extension_dir = \"ext\"" "extension_dir = \"ext\""
+%spn_fart% c:\php\php.ini ";extension=php_mysql.dll" "extension=php_mysql.dll"
