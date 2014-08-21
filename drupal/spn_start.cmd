e0ce8db287f7b0d8f0132b4bda812ef4b560c1dc
olegsomphane <oleg@somphane.com>
2014-08-21 12:31:13 -0700

    making start files more friendly for wp and dp

--- drupal/spn_start.cmd
+++ drupal/spn_start.cmd
@@ -1,2 +1,7 @@
+@ECHO OFF
+ECHO Starting mysql...
 start "mysqld" "%spn_mysqld%"
+ECHO Starting apache...
 start "apache" "%spn_apache%"
+ECHO Visit http://localhost:%spn_config_Port%/install.php to configure Drupal, if you haven't already.
+ECHO ON
