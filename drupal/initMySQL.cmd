da433087cfab26d7e5df3361aa327db985a002af
olegsomphane <oleg@somphane.com>
2014-08-22 10:17:48 -0700

    mysql script update

--- drupal/initMySQL.cmd
+++ drupal/initMySQL.cmd
@@ -3,15 +3,16 @@ start "mysql" mysqld.exe
 
 REM create database instruction file
 echo CREATE DATABASE drupal;> spn_cmd.txt
+REM create coutn variable for retries
 set spn_temp_count=0
 
 REM create database
 :createDB
-REM counter
+REM increment count
 set /A spn_temp_count=%spn_temp_count%+1
 REM sleep for 5 seconds
 ping -n 6 127.0.0.1 > nul
-REM pass database instruction file to to mysql
+REM pass database instruction file to toecho %ERR mysql
 mysql -u root < spn_cmd.txt
 REM if this fails more than 5 times, then fail the setup
 IF %spn_temp_count% GEQ 6 GOTO fail
@@ -22,9 +23,11 @@ goto createDB
 
 REM issue exit code on failure
 :fail
+echo MySQL setup failed.
 exit /b 1
 
 REM clean up and shutdown on success
 :succeed
+echo MySQL setup succeeded.
 del spn_cmd.txt
 mysqladmin.exe -u root shutdown
