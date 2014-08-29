4657de9e825a4315b4635f3163e8287625d78e6d
olegsomphane <oleg@somphane.com>
2014-08-29 10:30:17 -0700

    ghost (remove sqlite3 hack)

--- ghost/spn_start.cmd
+++ ghost/spn_start.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 ECHO Starting node.js...
 CD c:\ghost
-start "nodejs" npm start
+start "nodejs" npm start --production
 ECHO Edit config.js to configure Ghost, if you haven't already.
 ECHO ON
