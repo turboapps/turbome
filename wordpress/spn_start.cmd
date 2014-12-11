460556693a7b35ea0a65503b4640c45cf4b39d03
olegsomphane <oleg@somphane.com>
2014-12-11 11:53:55 -0800

    script update

--- wordpress/spn_start.cmd
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-ECHO Starting mysql...
-start "mysqld" "%spn_mysqld%"
-ECHO Starting apache...
-start "apache" "%spn_apache%"
-ECHO Visit http://localhost:%spn_config_Port% to configure WordPress, if you haven't already.
-ECHO ON
