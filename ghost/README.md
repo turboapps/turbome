589794ff370815cf45e27e6417c60134ffcb106d
olegsomphane <oleg@somphane.com>
2014-12-16 12:15:28 -0800

    Updating ghost script
    - update version
    - add readme
    - remove unneeded files
    - fix running from non-C drive

---
+++ ghost/README.md
@@ -0,0 +1,9 @@
+# Ghost
+
+Project website: https://ghost.org/
+
+Version: 0.5.7
+
+To build: 
+
+	spoon build -n=ghost /path/to/spoon.me
--- ghost/spn_start.cmd
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-ECHO Starting node.js...
-CD c:\ghost
-start "nodejs" npm start --production
-ECHO Edit config.js to configure Ghost, if you haven't already.
-ECHO ON
