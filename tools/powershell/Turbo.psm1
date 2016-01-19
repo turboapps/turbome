40ce05b2647945e81eecefccfbf5a87dc2019d44
pmateusz <mateusz@turbo.net>
2016-01-19 17:53:47 +0100

    Define container with basic TurboScript Tools, create Chocolatey based build for Remote Desktop Manager

---
+++ tools/powershell/Turbo.psm1
@@ -0,0 +1,18 @@
+﻿# Cmdlets used by TurboScript and snapshot scripts
+# Module should be saved on host machine in %SystemRoot%\system32\WindowsPowerShell\v1.0\Modules\Turbo directory
+
+function Get-LatestChocoPackageVersion($packageName)
+{
+    $versions = (& choco list $packageName | Where-Object {$_ -match "(?<version>\d+(:?\.\d+)+)"} | Select-Object -InputObject {$Matches['version']})
+    if($versions)
+    {
+        if($versions.Count -gt 1)
+        {
+            throw "More than one version is available: $packageName"
+        }
+        return $versions
+    }
+    throw "No versions available for package: $packageName"
+}
+
+Export-ModuleMember -Function 'Get-LatestChocoPackageVersion'
