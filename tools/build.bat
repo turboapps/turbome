684b91b79027eb7531c44cb85e4023071fc4cc33
pmateusz <mateusz@turbo.net>
2016-01-20 18:19:06 +0100

    Add functions for XAPPL modification to PowerShell module

--- tools/build.bat
+++ tools/build.bat
@@ -3,4 +3,4 @@
 for /f %%x in ('wmic path win32_localtime get /format:list ^| findstr "="') do set %%x
 set tag=%Year%.%Month%.%Day%
 
-turbo build turbo.me %tag% --mount %~dp0=C:\Scripts --admin --overwrite 
+turbo build turbo.me %tag% --mount %~dp0=C:\Scripts --overwrite 
