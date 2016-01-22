6beb049d4c4dc86c3926ad4f823210ed5fbdda57
pmateusz <mateusz@turbo.net>
2016-01-22 17:56:10 +0100

    Add TurboScript for WebEx Player. Extend PowerShell CmdLets to extract product version from Comments attribute

---
+++ tools/powershell/Turbo/Get-FileVersion.ps1
@@ -0,0 +1,47 @@
+﻿# Extract the latest version of a file
+# 
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+function Get-FileVersion
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+	    [string] $Path,
+        [Parameter(HelpMessage="Allows to extract version from different file metadata. Supported values are: FileVersion, Comments")]
+        [string] $Source = 'FileVersion'
+    )
+    process
+    {
+        if($Source -like 'FileVersion')
+        {
+            $tag = $Matches['version']
+            $versionInfo = (Get-Item -Path $Path).VersionInfo
+            return ("{0}.{1}.{2}.{3}" -f $versionInfo.FileMajorPart, 
+                $versionInfo.FileMinorPart, 
+                $versionInfo.FileBuildPart, 
+                $versionInfo.FilePrivatePart)
+        }
+        elseif($Source -like 'Comments')
+        {
+            $path = (Get-ChildItem $Path).FullName
+            $shell = New-Object -COMObject Shell.Application
+            $folder = Split-Path $path
+            $file = Split-Path $path -Leaf
+            $shellfolder = $shell.Namespace($folder)
+            $shellfile = $shellfolder.ParseName($file)
+
+            $CommentsKey = 0..287 | Where-Object { $shellfolder.GetDetailsOf($null, $_) -eq "Comments" }
+            $comment = $shellfolder.GetDetailsOf($shellfile, $CommentsKey)
+            if(-not ($comment -match '(?<version>\d+(?:\.\d+){3})')) {
+                throw "Failed to extract version number"
+            }
+
+            return $Matches['version']
+        }
+        
+        throw "Uknown Source: $Source"
+    }
+}
--- tools/powershell/Turbo/Turbo.psm1
+++ tools/powershell/Turbo/Turbo.psm1
@@ -3,7 +3,7 @@
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
 
-$cmdlets =  Get-ChildItem -Path @("$PSScriptRoot\Remove-BuildTools.ps1", "$PSScriptRoot\Get-LatestChocoVersion.ps1") -ErrorAction SilentlyContinue
+$cmdlets =  Get-ChildItem -Path @("$PSScriptRoot\Remove-BuildTools.ps1", "$PSScriptRoot\Get-LatestChocoVersion.ps1", "$PSScriptRoot\Get-FileVersion.ps1") -ErrorAction SilentlyContinue
 Foreach($cmdlet in $cmdlets)
 {
     Try
