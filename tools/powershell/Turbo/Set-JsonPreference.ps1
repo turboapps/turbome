3df8b5976c3ea164e18968e7cb7892d75401822f
pmateusz <mateusz@turbo.net>
2016-01-26 12:05:46 +0100

    Download Chromium Canary from a different location

---
+++ tools/powershell/Turbo/Set-JsonPreference.ps1
@@ -0,0 +1,37 @@
+# Edit JSON preference files
+# 
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+function Set-JsonPreference
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+	    [string] $Path,
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+	    [string] $Category,
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+	    [string] $Property,
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+	    [string] $Value
+    )
+    process
+    {
+        $json = (Get-Content $Path) | ConvertFrom-Json
+
+        $categoryValue = $json.PSobject.Properties | Where-Object {$_.Name -eq $Category}
+        if($categoryValue)
+        {
+            Add-Member -Name $Property -Value $Value -MemberType NoteProperty -InputObject $json.$Category -Force
+        }
+        else
+        {
+            $json | Add-Member -Name $category -Value @{ $Property = $Value } -MemberType NoteProperty
+        }
+
+        ConvertTo-Json $json -Compress | Set-Content $filepath
+        return $true
+    }
+}
