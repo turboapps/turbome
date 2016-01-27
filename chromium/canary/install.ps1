37a6de8fd371dbb806b45d47c816b625e9e52bda
pmateusz <mateusz@turbo.net>
2016-01-27 17:14:45 +0100

    Snapshot automation for Chromium Canary

---
+++ chromium/canary/install.ps1
@@ -0,0 +1,58 @@
+﻿#
+# Chromium Canary install script
+# https://github.com/turboapps/turbome/tree/master/chromium/canary
+#
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Download Canary binaries
+(New-Object System.Net.WebClient).DownloadFile(
+    "https://download-chromium.appspot.com/dl/Win?type=continuous",
+    "chromium-win32.zip")
+
+# Extract binaries to a fixed location
+$fullPath = Get-ChildItem -Path ".\chromium-win32.zip"
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+[System.IO.Compression.ZipFile]::ExtractToDirectory($fullPath, "c:\Chromium")
+
+# Deploy Turbo PowerShell module
+(New-Object System.Net.WebClient).DownloadFile(
+    "https://raw.githubusercontent.com/turboapps/turbome/master/tools/powershell/Turbo/Deploy-TurboModule.ps1",
+    "Deploy-TurboModule.ps1")
+. .\Deploy-TurboModule.ps1
+Deploy-TurboModule -Path "."
+Import-Module ".\Turbo"
+
+# Perform the first launch
+try
+{
+    Write-Host -Verbose "Launching Chromium for the first time"
+    $launchProcessTask = 'Start Chromium Canary'
+    Remove-ScheduledTask $launchProcessTask | Out-Null
+    Start-ProcessInScheduledTask -TaskName $launchProcessTask -Path "C:\Chromium\chrome-win32\chrome.exe"
+}
+finally
+{
+    Remove-ScheduledTask $launchProcessTask | Out-Null
+}
+
+try
+{
+    Write-Host -Verbose "Close Chromium window"
+    $closeProcessTask = 'Close Chromium Canary'
+    Remove-ScheduledTask $closeProcessTask | Out-Null
+    Close-WindowProcessInScheduledTask -TaskName $closeProcessTask -ProcessName chrome
+}
+finally
+{
+    Remove-ScheduledTask $closeProcessTask | Out-Null
+}
+
+# Set preferences
+Write-Host "Overwriting default preferences"
+$preferencesPath = "${env:LOCALAPPDATA}\Chromium\User Data\Default\Preferences"
+
+Set-JsonPreference -Path  $preferencesPath -Category 'browser' -Property 'check_default_browser' -Value $false | Out-Null
+Set-JsonPreference -Path  $preferencesPath -Category 'download' -Property 'prompt_for_download' -Value $true | Out-Null
+Set-JsonPreference -Path  $preferencesPath -Category 'download' -Property 'directory_upgrade' -Value $true | Out-Null
+Set-JsonPreference -Path  $preferencesPath -Category 'download' -Property 'extensions_to_open' -Value "" | Out-Null
