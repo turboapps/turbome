5fadf362eab238c41f1a0eda75deb96b474cf6c9
pmateusz <mateusz@turbo.net>
2016-02-04 12:21:24 +0100

    Clean up registry keys and turn off updates in TurboBase browser

---
+++ turbobrowsers/turbobase/post_snapshot.ps1
@@ -0,0 +1,34 @@
+﻿#
+# TurboBase post-snapshot script
+# https://github.com/turboapps/turbome/tree/master/turbobrowsers/turbobase
+#
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+Import-Module Turbo
+
+$XappPath = '.\output\Snapshot.xappl'
+$xappl = Read-XAPPL $XappPath
+
+Remove-Layer $xappl 'Xenocode'
+
+Remove-FileSystemDirectoryItems $xappl '@SYSTEM@'
+
+Remove-RegistryItems $xappl '@HKLM@\SYSTEM'
+Remove-RegistryItems $xappl '@HKCU@\SOFTWARE\Microsoft'
+Remove-RegistryItems $xappl '@HKLM@\SOFTWARE\Microsoft\TelemetryClient'
+
+Set-RegistryIsolation $xappl "@HKCU@\SOFTWARE\Classes" $FullIsolation
+Set-RegistryIsolation $xappl "@HKCU@\SOFTWARE\Clients" $FullIsolation
+Set-RegistryIsolation $xappl "@HKLM@\SOFTWARE\Classes" $FullIsolation
+Set-RegistryIsolation $xappl '@HKLM@\SOFTWARE\Microsoft\Windows' $FullIsolation
+@("", "\Wow6432Node") | ForEach-Object {
+    Set-RegistryIsolation $xappl "@HKLM@\SOFTWARE$_\Microsoft" $FullIsolation
+    Set-RegistryIsolation $xappl "@HKLM@\SOFTWARE$_\Mozilla" $FullIsolation
+    Set-RegistryIsolation $xappl "@HKLM@\SOFTWARE$_\mozilla.org" $FullIsolation
+    Set-RegistryIsolation $xappl "@HKLM@\SOFTWARE$_\RegisteredApplications" $FullIsolation
+}
+
+Set-RegistryValue $xappl -Path '@HKLM@\SOFTWARE\Wow6432Node\Mozilla\Firefox\TaskBarIDs' -Key '@PROGRAMFILESX86@\Mozilla Firefox' -Value ''
+
+Save-XAPPL $xappl $XappPath
