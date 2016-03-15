b648afd42cb951e7542c4deafb4e2699e571db70
pmateusz <mateusz@turbo.net>
2016-03-15 12:46:26 +0100

    Move building and pushing image to Jenkins plugin

--- turbobrowsers/block-ad-routes/ad-routes/run.ps1
+++ turbobrowsers/block-ad-routes/ad-routes/run.ps1
@@ -6,10 +6,3 @@
 # update routes.txt file
 & turbo build --overwrite "$PsScriptRoot\extract_routes.me" --mount $PsScriptRoot=C:\mount
 & turbo rmi extract_routes
-
-# build block-ad-routes image
-& turbo build --overwrite --route-file="$PsScriptRoot\routes.txt" "$PsScriptRoot\turbo.me"
-& turbo config --hub=stage.turbo.net
-& turbo push turbobrowsers/block-ad-routes
-& turbo config --hub=turbo.net
-& turbo push turbobrowsers/block-ad-routes
