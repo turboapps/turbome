b5e88ef5b5c8b430166dae5cd4fa7e6d8f6ab514
Rorymon <Rorymon@gmail.com>
2016-03-23 12:44:57 -0700

    upload scripts

---
+++ dell/powergui/postsnapshot.ps1
@@ -0,0 +1,46 @@
+#
+# Dell PowerGUI post-snapshot script
+# https://github.com/turboapps/turbome/tree/master/dell/powergui
+#
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+#
+# Edit XAPPL
+#
+
+Import-Module Turbo
+
+$XappPath = '.\output\Snapshot.xappl'
+$xappl = Read-XAPPL $XappPath
+
+
+Set-RegistryIsolation $xappl '@HKCU@\Software\Microsoft' $FullIsolation
+Set-RegistryIsolation $xappl '@HKLM@\SOFTWARE\Microsoft' $FullIsolation
+Set-RegistryIsolation $xappl '@HKLM@\SOFTWARE\Wow6432Node\Microsoft' $FullIsolation
+Set-RegistryIsolation $xappl '@HKLM@\SOFTWARE\Quest Software' $FullIsolation
+Set-RegistryIsolation $xappl '@HKLM@\SOFTWARE\Wow6432Node\Quest Software' $FullIsolation
+
+Remove-FileSystemItems $xappl '@APPDATACOMMON@\chocolatey'
+Remove-FileSystemItems $xappl '@APPDATALOCAL@\NuGet'
+Remove-FileSystemItems $xappl '@APPDATALOCALLOW@\Microsoft'
+Remove-FileSystemItems $xappl '@SYSTEM@\CodeIntegrity'
+Remove-FileSystemDirectoryItems $xappl '@SYSDRIVE@'
+
+Import-Files $xappl -SourceDir '.\Resources\Files' -SnapshotDir '.\output\Files' | Out-Null
+
+$powerguiInstallDir = '.\output\Files\Default\@PROGRAMFILESX86@\PowerGUI'
+
+Remove-StartupFiles $xappl
+$powerguiscriptExe = '@PROGRAMFILESX86@\PowerGUI\ScriptEditor.exe'
+$powerguiscriptx86Exe = '@PROGRAMFILESX86@\PowerGUI\ScriptEditor_x86.exe'
+$powerguiadminExe = '@PROGRAMFILESX86@\PowerGUI\AdminConsole.exe'
+$powerguiadminx86Exe = '@PROGRAMFILESX86@\PowerGUI\AdminConsole_x86.exe'
+Add-StartupFile $xappl -File $powerguiscriptExe -Name 'ScriptEditor' -AutoStart
+Add-StartupFile $xappl -File $powerguiscriptx86Exe -Name 'ScriptEditor_x86'
+Add-StartupFile $xappl -File $powerguiadminExe -CommandLine '-AuthoringMode' -Name 'AdminConsoleAuthoring'
+Add-StartupFile $xappl -File $powerguiadminx86Exe -CommandLine '-AuthoringMode' -Name 'AdminConsoleAuthoringx86'
+Add-StartupFile $xappl -File $powerguiadminExe -Name 'AdminConsole'
+Add-StartupFile $xappl -File $powerguiadminx86Exe-Name 'AdminConsolex86'
+
+Save-XAPPL $xappl $XappPath
---
+++ dell/powergui/setup.ps1
@@ -0,0 +1,10 @@
+#
+# Dell PowerGUI setup script
+# https://github.com/turboapps/turbome/tree/master/dell/powergui
+#
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+Import-Module Turbo
+$version = (Get-NextTag "dell/powershellgui")
+"dell/powershellgui:$version" | Set-Content image.txt
