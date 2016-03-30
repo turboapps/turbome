9dccf8508e0645a26dfe5620841391360475abcd
Rorymon <Rorymon@gmail.com>
2016-03-30 16:00:44 -0700

    Removed Steam reference in headers

--- vmware/vsphereclient/install.ps1
+++ vmware/vsphereclient/install.ps1
@@ -6,7 +6,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 
-# Intall Steam binaries
+# Intall vSphere Client binaries
 & C:\vagrant\install\installer.exe /S | Write-Host
 
 
@@ -19,7 +19,7 @@ Deploy-TurboModule -Path "."
 Import-Module ".\Turbo"
 
 
-# Get Steam version
+# Get vSphere Client version
 $version = Get-FileVersion "${Env:ProgramFiles(x86)}\VMware\Infrastructure\Virtual Infrastructure Client\Launcher\VpxClient.exe"
 Write-Output "Steam version after update: $version"
 Set-Content 'C:\vagrant\image.txt' "steam/steam:$version"
