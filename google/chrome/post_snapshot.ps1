2a66b905cb42468a658290647998df6591d4cce6
pmateusz <mateusz@turbo.net>
2016-04-25 14:43:08 +0200

    Set merge isolation for native message host in Chrome snapshot

--- google/chrome/post_snapshot.ps1
+++ google/chrome/post_snapshot.ps1
@@ -18,6 +18,11 @@ $virtualizationSettings = $xappl.Configuration.VirtualizationSettings
 $virtualizationSettings.isolateWindowClasses = [string]$true
 $virtualizationSettings.launchChildProcsAsUser = [string]$true
 
+# set net.spoon.chromenativehost key to merge isolation, so Turbo VM Extension is able to establish connection with a message host installed natively 
+$NativeMessageHostKey = "@HKCU@\SOFTWARE\Google\Chrome\NativeMessagingHosts\net.spoon.chromenativehost"
+Add-RegistryKey $xappl $NativeMessageHostKey $FullIsolation
+Set-RegistryIsolation $xappl $NativeMessageHostKey $MergeIsolation
+
 Set-FileSystemIsolation $xappl "@APPDATACOMMON@\Microsoft" $FullIsolation
 Set-FileSystemIsolation $xappl "@APPDATALOCAL@\Google" $FullIsolation
 Set-FileSystemIsolation $xappl "@PROGRAMFILESX86@\Google" $FullIsolation
