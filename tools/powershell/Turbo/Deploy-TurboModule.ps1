c63459d567de705b7182d11d9795bb515b4356d2
pmateusz <mateusz@turbo.net>
2016-04-28 15:30:20 +0200

    Make method compatible with PowerShell 2.0

--- tools/powershell/Turbo/Deploy-TurboModule.ps1
+++ tools/powershell/Turbo/Deploy-TurboModule.ps1
@@ -19,10 +19,35 @@ function Deploy-TurboModule
         }
         $fullModulePath = (Resolve-Path $relativeModulePath).Path
 
-        $fileItems = Invoke-RestMethod https://api.github.com/repos/turboapps/turbome/contents/tools/powershell/Turbo
+        # Invoke-WebRequest is not available in PowerShell 2.0
+        function Invoke-WebRequest20($url)
+        {
+            [System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions") | Out-Null
+            
+            # Create request manually to avoid 'The server committed a protocol violation. Section=ResponseStatusLine'
+            $request = [System.Net.HttpWebRequest]::Create($url)
+            $request.ContentType = "application/json"
+            $request.Method = "GET"
+            $request.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64)"
+            
+            $response = $request.GetResponse()
+            $streamReader = New-Object System.IO.StreamReader $response.GetResponseStream()
+            try
+            {
+                $json = $streamReader.ReadToEnd()
+                $serializer = New-Object System.Web.Script.Serialization.JavaScriptSerializer
+                return $serializer.DeserializeObject($json)
+            }
+            finally
+            {
+                $streamReader.Dispose()    
+            }
+        }
+
         $webClient = New-Object System.Net.WebClient
         try
         {
+            $fileItems = Invoke-WebRequest20 "https://api.github.com/repos/turboapps/turbome/contents/tools/powershell/Turbo"
             foreach($fileItem in $fileItems)
             {
                 $webClient.DownloadFile($fileItem.download_url, "$fullModulePath\$($fileItem.name)")
