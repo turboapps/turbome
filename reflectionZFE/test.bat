62a054ea5a99440595f9727aabf43d619756ad4a
Michał Kowalik <michal@kowalik.net>
2016-11-10 17:34:04 +0100

    firefox path changed to use system env variable

--- reflectionZFE/test.bat
+++ reflectionZFE/test.bat
@@ -1,14 +1,15 @@
+set FF=%programfiles(x86)%\Mozilla Firefox\firefox.exe
 IF EXIST run.txt (
-"C:\Program Files (x86)\Mozilla Firefox\firefox.exe" C:\MSS\utilities\AdministrativeWebStation.html
+"%FF%" C:\MSS\utilities\AdministrativeWebStation.html
 ) ELSE (
 sc stop "Micro Focus MSS Server"
 START /WAIT C:\MSS\utilities\bin\InitialConfigurationUtility.exe
 type NUL>run.txt
 sc start "Micro Focus MSS Server"
-cd "C:\Program Files (x86)\Mozilla Firefox"
+cd %programfiles(x86)%\Mozilla Firefox
 START /WAIT firefox C:\ZFEInstaller\manual.html C:\MSS\utilities\AdministrativeWebStation.html
 C:\ZFEInstaller\reflectionzfe-2.0.0.51-windows-x64.exe -q -c -varfile C:\ZFEInstaller\response.varfile
-"C:\Program Files (x86)\Mozilla Firefox\firefox.exe" C:\MSS\utilities\AdministrativeWebStation.html
+firefox C:\MSS\utilities\AdministrativeWebStation.html
 )
 
 
