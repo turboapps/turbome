631d5cacf7f194100f7366f72973eba0f43dfdb4
kmichael <michal@kowalik.net>
2017-11-13 14:28:05 +0100

    PowerBI initial commit

---
+++ microsoft/powerbi/install.ps1
@@ -0,0 +1,21 @@
+#
+# PowerBI install file
+# https://github.com/turboapps/turbome/tree/master/microsoft/powerbi
+#
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+#Install PowerBI
+& msiexec /i "X:\install\install.msi" /qn /norestart ACCEPT_EULA=1 | Out-Null
+
+#Disable settings in registry
+$RegKey = "HKCU:\Software\Microsoft\Microsoft Power BI Desktop\"
+Set-ItemProperty -Path $RegKey -Name ShowLeadGenDialog -Value 0
+Set-ItemProperty -Path $RegKey -Name EnableCustomerExperienceProgram -Value 0
+
+
+$mainExe = "$env:PROGRAMFILES\Microsoft Power BI Desktop\bin\PBIDesktop.exe"
+Write-Host "Version info"
+(Get-Item $mainExe).VersionInfo
+$version = (Get-Item $mainExe).VersionInfo.FileVersion
+"microsoft/powerbi:$version" | Set-Content "X:\output\image.txt"
---
+++ microsoft/powerbi/setup.ps1
@@ -0,0 +1,15 @@
+#
+# PowerBI snapshot setup file
+# https://github.com/turboapps/turbome/tree/master/microsoft/powerbi
+#
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+if(!(Test-Path ".\installFiles")) { New-Item ".\installFiles" -type directory}
+
+$website = Invoke-WebRequest -Uri https://powerbi.microsoft.com/en-us/desktop/
+$item = $website.links | where {$_.innerText -like "*Download free*"}
+$link = "https:" + $item[0].href
+$link = $link -replace "amp;",""
+Write-Host "Fixed link is: " + $link
+(New-Object System.Net.WebClient).DownloadFile($link, ".\installFiles\install.msi")
