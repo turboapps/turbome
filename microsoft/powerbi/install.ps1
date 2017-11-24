e37e6e6592caeaf95b71d252e86d57714e9e6bb9
kmichael <michal@kowalik.net>
2017-11-24 16:28:47 +0100

    PowerBI - disable update notifications

--- microsoft/powerbi/install.ps1
+++ microsoft/powerbi/install.ps1
@@ -6,12 +6,11 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 #Install PowerBI
-& msiexec /i "X:\install\install.msi" /qn /norestart ACCEPT_EULA=1 | Out-Null
+& msiexec /i "X:\install\install.msi" /qn /norestart ACCEPT_EULA=1 DISABLE_UPDATE_NOTIFICATION=1 ENABLECXP=0 | Out-Null
 
 #Disable settings in registry
 $RegKey = "HKCU:\Software\Microsoft\Microsoft Power BI Desktop\"
 Set-ItemProperty -Path $RegKey -Name ShowLeadGenDialog -Value 0
-Set-ItemProperty -Path $RegKey -Name EnableCustomerExperienceProgram -Value 0
 
 
 $mainExe = "$env:PROGRAMFILES\Microsoft Power BI Desktop\bin\PBIDesktop.exe"
