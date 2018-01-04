a01db2dc3adb4f43cb168176e0faa495b5a97f10
kmichael <michal@kowalik.net>
2018-01-04 13:24:57 +0100

    PowerBI - Disable stylus and touch support to fix ribbon bug. CI-239

--- microsoft/powerbi/install.ps1
+++ microsoft/powerbi/install.ps1
@@ -12,6 +12,19 @@
 $RegKey = "HKCU:\Software\Microsoft\Microsoft Power BI Desktop\"
 Set-ItemProperty -Path $RegKey -Name ShowLeadGenDialog -Value 0
 
+#Disable stylus and touch support
+# This is because there were some issues with Power BI's ribbon UI hanging and there was a PenIMC.dll-related exception.
+# We downgraded .NET to 4.6.1 on the RDP servers suspecting it was the same issue as
+# https://developercommunity.visualstudio.com/content/problem/55303/visual-studio-may-terminate-unexpectedly-when-runn.html
+# but better be safe than sorry and also disable touch support in the app itself.
+
+$exeConfigPath = "$env:programfiles\Microsoft Power BI Desktop\bin\PBIDesktop.exe.config"
+$exeConfig = Get-Content $exeConfigPath -Raw
+$oldString = '<runtime>'
+$newString = '<runtime>
+    <AppContextSwitchOverrides value="Switch.System.Windows.Input.Stylus.DisableStylusAndTouchSupport=true" />'
+$exeConfig = $exeConfig.Replace($oldString, $newString)
+$exeConfig | Out-File -FilePath $exeConfigPath -Force -Encoding utf8
 
 $mainExe = "$env:PROGRAMFILES\Microsoft Power BI Desktop\bin\PBIDesktop.exe"
 Write-Host "Version info"
