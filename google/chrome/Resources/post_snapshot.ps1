48d2eee708dfb689312439f5d887894dc9f42498
kmichael <michal@kowalik.net>
2018-03-20 17:43:53 +0100

    chrome registry fix

--- google/chrome/Resources/post_snapshot.ps1
+++ google/chrome/Resources/post_snapshot.ps1
@@ -18,7 +18,7 @@ $virtualizationSettings = $xappl.Configuration.VirtualizationSettings
 $virtualizationSettings.isolateWindowClasses = [string]$true
 $virtualizationSettings.launchChildProcsAsUser = [string]$true
 
-# set net.spoon.chromenativehost key to merge isolation, so Turbo VM Extension is able to establish connection with a message host installed natively 
+# set net.spoon.chromenativehost key to merge isolation, so Turbo VM Extension is able to establish connection with a message host installed natively
 $NativeMessageHostKey = "@HKCU@\SOFTWARE\Google\Chrome\NativeMessagingHosts\net.spoon.chromenativehost"
 Add-RegistryKey $xappl $NativeMessageHostKey $FullIsolation
 Set-RegistryIsolation $xappl $NativeMessageHostKey $MergeIsolation
@@ -40,7 +40,13 @@ Set-RegistryIsolation $xappl "@HKLM@\SOFTWARE\Wow6432Node\Google" $FullIsolation
 
 Remove-RegistryItems $xappl "@HKCU@\Software\Microsoft"
 Remove-RegistryItems $xappl "@HKLM@\SOFTWARE\Wow6432Node\Microsoft"
-Remove-RegistryItems $xappl "@HKLM@\SOFTWARE\Microsoft"
+#This is to keep @HKLM@\SOFTWARE\Microsoft\CurrentVersion\App Paths but delete everything else from @HKLM@\SOFTWARE\Microsoft
+Remove-RegistryItems $xappl "@HKLM@\SOFTWARE\Microsoft\Active Setup"
+Remove-RegistryItems $xappl "@HKLM@\SOFTWARE\Microsoft\Internet Explorer"
+Remove-RegistryItems $xappl "@HKLM@\SOFTWARE\Microsoft\MediaPlayer"
+Remove-RegistryItems $xappl "@HKLM@\SOFTWARE\Microsoft\Windows NT"
+Remove-RegistryItems $xappl "@HKLM@\SOFTWARE\Microsoft\CurrentVersion\Installer"
+Remove-RegistryItems $xappl "@HKLM@\SOFTWARE\Microsoft\CurrentVersion\Uninstaller"
 
 Add-ObjectMap $xappl -Name 'window://Chrome_MessageWindow:0'
 
