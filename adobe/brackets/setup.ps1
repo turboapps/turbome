e1bb54c00d951e25e065f1ffd306975405051fd3
kmichael <michal@kowalik.net>
2018-04-09 15:15:39 +0200

    Adobe Brackets initial commit

---
+++ adobe/brackets/setup.ps1
@@ -0,0 +1,17 @@
+﻿#
+# Chromium Canary setup script
+# https://github.com/turboapps/turbome/tree/master/chromium/canary
+#
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$result = Invoke-WebRequest -Uri https://github.com/adobe/brackets/releases
+if ($result -NotMatch 'a href="(?<downloadLink>.*.msi)"')
+	{ throw "Failed to extract download link" }
+$link = "https://github.com/"+$Matches['downloadLink']
+$version = [regex]::match($link,'[0-9]+.[0-9]+').Value
+
+(New-Object System.Net.WebClient).DownloadFile($link,"install.msi")
+
+"adobe/brackets:$version" | Set-Content "image.txt"
