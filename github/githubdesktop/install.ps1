344f0451c4f8f9a6929e8873b160d6c9829e27d2
kmichael <michal@kowalik.net>
2018-04-13 16:06:14 +0200

    GitHub Desktop initial commit

---
+++ github/githubdesktop/install.ps1
@@ -0,0 +1,17 @@
+﻿#
+# GitHub Dekstop install script
+# https://github.com/turboapps/turbome/tree/master/github/githubdesktop
+#
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+#Install Github Desktop
+& "X:\install\install.exe" | Out-Null
+Start-Sleep -Seconds 10
+Stop-Process -Name "github*"
+
+#Remove updater
+$appdatapath = $env:LOCALAPPDATA
+Get-ChildItem -Path $appdatapath -Filter "*squirrel*" -Recurse | Remove-Item -Recurse
+$updaterPath = $appdatapath+"\GitHubDesktop\Update.exe"
+Remove-Item $updaterPath
---
+++ github/githubdesktop/setup.ps1
@@ -0,0 +1,18 @@
+﻿#
+# GitHub Dekstop setup script
+# https://github.com/turboapps/turbome/tree/master/github/githubdesktop
+#
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+$page = Invoke-WebRequest -Uri https://desktop.github.com/
+$link = ($page.Links.href | where {$_ -like "*win32"})[0]
+
+if(!(Test-Path ".\installFiles")) { New-Item ".\installFiles" -type directory}
+
+$path = ".\installFiles\install.exe"
+(New-Object System.Net.WebClient).DownloadFile($link, $path)
+
+$version = (Get-Item $path).VersionInfo.FileVersion
+
+"github/githubdesktop:$version" | Set-Content "image.txt"
