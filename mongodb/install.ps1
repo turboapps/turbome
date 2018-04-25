87e5a007628a2808f3071dddc8c1d0603142a830
kmichael <michal@kowalik.net>
2018-04-25 13:12:58 +0200

    MongoDB link extraction fix

--- mongodb/install.ps1
+++ mongodb/install.ps1
@@ -1,9 +1,9 @@
 ﻿#
-#  install script
+# MongoDB install script
 # https://github.com/turboapps/turbome/tree/master/mongodb
 #
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
 
-#Install Brackets
+#Install
 & msiexec /i "X:\install\install.msi" /qn /norestart | Out-Null
--- mongodb/setup.ps1
+++ mongodb/setup.ps1
@@ -7,6 +7,9 @@
 
 $page = Invoke-WebRequest -Uri "https://www.mongodb.com/download-center?jmp=nav#community"
 $link = "https://mongodb.com"+( $page.Links.href -Match "signed.msi")
+$link = $link -replace "/dr/", "http://"
+$link = $link -replace "/download$",""
+
 $version = [regex]::match($link,'[0-9]+\.[0-9]+\.[0-9]+').Value
 
 if(!(Test-Path ".\installFiles")) { New-Item ".\installFiles" -type directory}
@@ -14,3 +17,6 @@ if(!(Test-Path ".\installFiles")) { New-Item ".\installFiles" -type directory}
 (New-Object System.Net.WebClient).DownloadFile($link, ".\installFiles\install.msi")
 
 "mongodb/mongo:$version" | Set-Content "image.txt"
+
+https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-3.6.4-signed.msi
+https://www.mongodb.com/dr/fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-3.6.4-signed.msi/download
