360247641ed383080d8a882d377bc2d986a29694
kmichael <michal@kowalik.net>
2018-08-03 15:59:52 +0200

    Office 2016 build scripts initial commit

---
+++ microsoft/office/buildScript.ps1
@@ -0,0 +1,167 @@
+function Capture-Before {
+    & "c:\share\tools\XStudio.exe" /before /beforepath c:\output\snapshot
+}
+function Capture-After {
+    & "c:\share\tools\XStudio.exe" /after /beforepath c:\output\snapshot /o c:\output
+}
+
+function Build-Image {
+    & "c:\share\tools\XStudio.exe" c:\output\snapshot.xappl /o c:\share\output\image.svm /component /uncompressed /l c:\share\tools\license.txt
+}
+
+function Send-Keystroke{
+    param(
+    [Parameter(Mandatory=$true)]
+    [String]$Keystroke
+    )
+    
+    [Windows.Forms.Sendkeys]::SendWait($Keystroke)
+}
+
+function Install-Office {
+    $opticalDrivePath = (Get-CimInstance -class cim_cdromdrive).Drive
+    $officeInstallerPath = "C:\Installer"
+    New-Item $officeInstallerPath -type directory
+    Copy-Item "$opticalDrivePath\*" $officeInstallerPath -Recurse
+    Copy-Item "c:\share\install\*.msp" "$officeInstallerPath\updates"
+
+    & "$officeInstallerPath\setup.exe" | Out-Null
+}
+function Find-OfficeExe {
+    $programFiles86Dir = (${env:ProgramFiles(x86)}, ${env:ProgramFiles} -ne $null)[0]
+    $officeExeDirectory = "$programFiles86Dir\Microsoft Office\Office16"
+    if (Test-Path "$officeExeDirectory\EXCEL.EXE")
+    {
+        $exeToLaunch = "EXCEL"
+    }
+    if (Test-Path "$officeExeDirectory\POWERPNT.EXE")
+    {
+        $exeToLaunch = "POWERPNT"
+    }
+    if (Test-Path "$officeExeDirectory\WINWORD.EXE")
+    {
+        $exeToLaunch = "WINWORD"
+    }
+
+    return "$officeExeDirectory\$exeToLaunch.EXE"
+}
+
+function FirstLaunch-Office {
+    $officeExe = Find-OfficeExe
+
+    & $officeExe
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+    Sleep -s 30
+    Send-Keystroke " "
+    Sleep -Milliseconds 100
+    Send-Keystroke "{DOWN}"
+    Sleep -Milliseconds 100
+    Send-Keystroke "{ENTER}"
+    Sleep -Milliseconds 500
+
+    $exeToStop = $officeExe.split('\.')[-2]
+    Get-Process $exeToStop | Stop-Process -Force
+}
+
+function Get-OfficeVersion {
+    $officeExePath = Find-OfficeExe
+    $officeExe = Get-Item -path $officeExePath
+    $officeExe.VersionInfo.FileVersion | Out-File c:\share\output\version.txt
+}
+
+function Enable-OfficeUpdatesInWinUpdate {
+    $ServiceManager = New-Object -ComObject "Microsoft.Update.ServiceManager"
+    $ServiceManager.ClientApplicationID = "My App"
+    $ServiceManager.AddService2( "7971f918-a847-4430-9279-4a52d1efe18d",7,"")
+}
+
+function Install-Updates {
+    $AvailableUpdates = @()
+    Write-Host "Creating Update Session"
+    $Session = New-Object -com "Microsoft.Update.Session"
+    $Search = $Session.CreateUpdateSearcher()
+    $SearchResults = $Search.Search("IsInstalled=0 and IsHidden=0")
+
+    Write-Host "There are " $SearchResults.Updates.Count "TOTAL updates available."
+    $SearchResults.Updates | ForEach-Object {
+        if(($_.Categories.Item(0).Name -like "Office 2016") -or ($_.Categories.Item(1).Name -like "Office 2016")) {
+            $AvailableUpdates += $_
+        }
+    }
+
+    Write-Host "There are " $AvailableUpdates.Count "Office updates available:"
+    $AvailableUpdates | ForEach-Object {
+        Write-Host $_.Title -ForegroundColor Green
+    }
+
+    Write-Host "Downloading updates..."
+    $DownloadCollection = New-Object -com "Microsoft.Update.UpdateColl"
+    $AvailableUpdates | ForEach-Object {
+        if ($_.InstallationBehavior.CanRequestUserInput -ne $TRUE) {
+            $DownloadCollection.Add($_) | Out-Null
+            }
+        }
+    $Downloader = $Session.CreateUpdateDownloader()
+    $Downloader.Updates = $DownloadCollection
+    $Downloader.Download()
+
+    Write-Host "Download complete."
+
+    Write-Host "Creating Installation Object"
+    $InstallCollection = New-Object -com "Microsoft.Update.UpdateColl"
+    $AvailableUpdates | ForEach-Object {
+        if ($_.IsDownloaded) {
+            $InstallCollection.Add($_) | Out-Null
+        }
+    }
+
+    Write-Host "Installing updates..."
+    $Installer = $Session.CreateUpdateInstaller()
+    $Installer.Updates = $InstallCollection
+    $Results = $Installer.Install()
+    Write-Host "Installation complete."
+
+    # Reboot if needed
+    if ($Results.RebootRequired) {
+        Write-Host "Please reboot."
+    }
+    else {
+        Write-Host "No reboot required."
+    }
+}
+
+function Update-Office {
+    Enable-OfficeUpdatesInWinUpdate
+
+    Install-Updates
+}
+
+function Configure-Snapshot {
+    Import-Module C:\share\tools\Turbo
+
+    $XappPath = 'C:\output\Snapshot.xappl'
+    $xappl = Read-XAPPL $XappPath
+
+    $virtualizationSettings = $xappl.Configuration.VirtualizationSettings
+    $virtualizationSettings.launchChildProcsAsUser = [string]$true
+    $virtualizationSettings.faultExecutablesIntoSandbox = [string]$true
+
+    Save-XAPPL $xappl $XappPath
+
+}
+
+Capture-Before
+
+Install-Office
+
+FirstLaunch-Office
+
+Update-Office
+
+Capture-After
+
+Configure-Snapshot
+
+Build-Image
+
+Get-OfficeVersion
---
+++ microsoft/office/vmHarness.ps1
@@ -0,0 +1,179 @@
+[CmdletBinding()]
+param
+(   
+    [Parameter(Mandatory=$True)]
+    [string] $workspacePath,
+
+    [Parameter(Mandatory=$True)]
+    [string] $studioHomePath,
+
+    [Parameter(Mandatory=$True)]
+    [string] $studioLicenseFile,
+
+    [Parameter(Mandatory=$True)]
+    [string] $PsExecPath,
+
+    [Parameter(Mandatory=$True)]
+    [string] $turboModulePath,
+
+    [Parameter(Mandatory=$True)]
+    [string] $machine,
+
+    [Parameter(Mandatory=$True)]
+    [string] $machineIp,
+
+    [Parameter(Mandatory=$True)]
+    [string] $user,
+
+    [Parameter(Mandatory=$True)]
+    [string] $pass,
+
+    [Parameter(Mandatory=$True,ValueFromPipeline=$False,ValueFromPipelineByPropertyName=$False,HelpMessage="Build script path")]
+    [string] $buildScript,
+
+    [Parameter(Mandatory=$True,ValueFromPipeline=$False,ValueFromPipelineByPropertyName=$False,HelpMessage="Build script path")]
+    [string] $configurationMSPFilePath,
+
+    [Parameter(Mandatory=$False)]
+    [string] $officeIsoPath = "C:\CI\ISO\Office2016_ProPlus.ISO",
+
+    [Parameter(Mandatory=$False)]
+    [string] $virtualboxDir = "C:\Program Files\Oracle\VirtualBox"
+)
+
+function Clean-Workspace {
+    Remove-Item "$workspacePath\*" -Recurse -Force
+}
+
+function Prepare-SharedDirectory {
+    New-Item "$workspacePath\share" -type directory
+    New-Item "$workspacePath\share\install" -type directory
+    New-Item "$workspacePath\share\tools" -type directory
+    New-Item "$workspacePath\share\output" -type directory
+
+    #Copy files to shared directory
+    Copy-Item "$buildScript" "$workspacePath\share\install\buildScript.ps1"
+    Copy-Item "$studioHomePath\xstudio.exe" "$workspacePath\share\tools\xstudio.exe"
+    Copy-Item "$studioHomePath\StudioDependencies.svm" "$workspacePath\share\tools\StudioDependencies.svm"
+    Copy-Item "$studioHomePath\$studioLicenseFile" "$workspacePath\share\tools\license.txt"
+    Copy-Item "$turboModulePath" "$workspacePath\share\tools" -Recurse
+
+    Copy-Item "$configurationMSPFilePath" "$workspacePath\share\install"
+}
+
+function Configure-VirtualMachine {
+    Write-Host "Checking VM state"
+    $vmstate = & "$virtualboxDir\VBoxManage.exe" showvminfo $machine | Select-String -Pattern "State:" | Out-String
+    if(-Not ($vmstate -match "powered off"))
+    {
+        #Turn virtual machine off
+        "Virtual Machine is not powered off. Doing it now."
+        & "$virtualboxDir\VBoxManage.exe" controlvm $machine poweroff
+        Sleep -s 15
+    }
+    
+    Sleep -s 5
+    Write-Host "Restoring snapshot"
+    & "$virtualboxDir\VBoxManage.exe" snapshot $machine restore "turboBuildNetworking"
+    Sleep -s 5
+    Write-Host "Adding shared directory"
+    & "$virtualboxDir\VBoxManage.exe" sharedfolder add $machine --name turboBuild --hostpath (Get-Item "$workspacePath\share").FullName
+    Sleep -s 5
+    Write-Host "Attaching ISO"
+    & "$virtualboxDir\VBoxManage.exe" storageattach $machine --storagectl "IDE Controller" --type dvddrive --medium $officeIsoPath --port 1 --device 0
+    Sleep -s 5
+}
+
+function Prepare-BatFile {
+    $logForwardScript = "net use x: \\vboxsrv\turboBuild
+    :loop
+    timeout /t 20 >nul
+    copy /b/v/y C:\share\output\logStd.txt X:\output\logStd.txt
+    copy /b/v/y C:\share\output\logErr.txt X:\output\logErr.txt
+    copy /b/v/y C:\share\output\version.txt X:\output\version.txt
+    goto loop"
+    $installScript = "net use x: \\vboxsrv\turboBuild
+    xcopy X:\ C:\share /se /I /y
+    START /WAIT powershell -command `"&{start-process powershell -verb RunAs -ArgumentList \`"start-process powershell -argumentList '-File C:\share\install\buildScript.ps1' -RedirectStandardOutput C:\share\output\logStd.txt -RedirectStandardError C:\share\output\logErr.txt\`"}`""
+    $exportScript = "net use x: \\vboxsrv\turboBuild
+    xcopy C:\share\output X:\output /se /I /y"
+
+    [System.IO.File]::WriteAllLines("$workspacePath\installScript.bat", $installScript)
+    [System.IO.File]::WriteAllLines("$workspacePath\logForwardScript.bat", $logForwardScript)
+    [System.IO.File]::WriteAllLines("$workspacePath\exportScript.bat", $exportScript)
+}
+
+function Wait-VirtualMachine {
+    $timeout = 300
+    Write-Host "Test System: $machine"
+    Write-Host "Timeout: $timeout"
+
+    $span = new-timespan -Seconds $timeout
+    $timer = [diagnostics.stopwatch]::StartNew()
+    While ($True)
+    {
+    If ($timer.elapsed -gt $span)
+    {
+      Write-Error "Error: Timeout of $timeout seconds reached while waiting for $machine."
+      return -1
+    }
+
+    # just execute a prompt window and close it immediately as a test for connectivity
+    $p = Start-Process -FilePath $PsExecPath -ArgumentList "\\$machineIp -u $user -p $pass cmd.exe /c" -Wait -NoNewWindow -PassThru
+    If ($p.ExitCode -eq 0)
+    {
+      break
+    }
+
+    Write-Host "$machine is not ready yet."
+    Start-Sleep -Seconds 5
+    }
+
+    Write-Host "$machine is ready!"
+    return 0
+}
+
+function Restore-VirtualMachine {
+    Write-Host "Restoring $machine to the base snapshot"
+    & "$virtualboxDir\VBoxManage.exe" snapshot $machine restore "turboBuild"
+}
+
+Import-Module Turbo
+
+Clean-Workspace
+Sleep -s 5
+
+Prepare-SharedDirectory
+Sleep -s 5
+
+Configure-VirtualMachine
+Sleep -s 5
+
+Prepare-BatFile
+Sleep -s 5
+
+Write-Host "Start VM"
+& "$virtualboxDir\VBoxManage.exe" startvm $machine
+
+Wait-VirtualMachine
+
+Write-Host "Running log script from $workspacePath\logForwardScript.bat on $machine"
+& $PsExecPath "\\$machineIp" -u $user -p $pass -i -h -d -c "$workspacePath\logForwardScript.bat"
+
+Write-Host "Running install script from $workspacePath\installScript.bat on $machine"
+& $PsExecPath "\\$machineIp" -u $user -p $pass -i -h -c "$workspacePath\installScript.bat"
+
+Write-Host "Wait for the build to finish"
+while (!(Test-Path "$workspacePath\share\output\version.txt"))
+{
+    Sleep 30
+}
+
+Write-Host "Running export script from $workspacePath\exportScript.bat on $machine"
+& $PsExecPath "\\$machineIp" -u $user -p $pass -i -h -c "$workspacePath\exportScript.bat"
+
+Write-Host "Shutdown the $machine VM"
+& "$virtualboxDir\VBoxManage.exe" controlvm $machine poweroff
+
+Sleep -s 5
+Restore-VirtualMachine
