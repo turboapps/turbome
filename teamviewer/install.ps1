5d6da8964273a39a8c24928c6ac985eda65367de
Michal Kowalik <michal@kowalik.net>
2018-10-24 15:16:01 +0200

    Teamviewer - move to turboscript

--- teamviewer/install.ps1
+++
@@ -1,9 +0,0 @@
-#
-# TeamViewer install script
-# https://github.com/turboapps/turbome/tree/master/teamviewer
-#
-# Licensed under the Apache License, Version 2.0
-# http://www.apache.org/licenses/LICENSE-2.0
-
-# Install TeamViewer Chocolatey package
-& choco install teamviewer --x86 --confirm
