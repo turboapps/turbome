db95f270ebc3d3560c45f64b60d0d19e6e45422d
Michal Kowalik <michal@kowalik.net>
2018-10-30 16:22:45 +0100

    MongoDB script update - link change

--- mongodb/setup.ps1
+++ mongodb/setup.ps1
@@ -6,12 +6,13 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$page = Invoke-WebRequest -Uri "https://www.mongodb.com/download-center?jmp=nav#community"
-$link = ( $page.Links.href -Match "signed.msi")[0]
-$link = $link -replace "/dr/", "http://"
-$link = $link -replace "/download$",""
-
-$version = [regex]::match($link,'[0-9]+\.[0-9]+\.[0-9]+').Value
+$page = Invoke-WebRequest -Uri "https://www.mongodb.com/download-center/community?jmp=nav"
+$page -match "\d+\.\d+\.\d+(?= \(current release\))" | Out-Null
+$version = $Matches[0]
+$page -match "https://fastdl.mongodb.org/win32[a-z0-9\.\/\-_]*$version\.zip" | Out-Null
+$link = $Matches[0]
+$link = $link -replace "\.zip", "-signed.msi"
+Write-Host $link
 
 if(!(Test-Path ".\installFiles")) { New-Item ".\installFiles" -type directory}
 
