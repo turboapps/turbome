0eb5f0d6394567c862cd3a716c935e1c9bf67acf
Michael Rush <rush@turbo.net>
2023-03-09 17:04:09 -0800

    Whisper image only transcribed the first 30 seconds of audio

--- whisper/resources/whisper_caller.py
+++ whisper/resources/whisper_caller.py
@@ -1,26 +1,11 @@
 import whisper
-import sys
 
 def transcribe(audioFile, outputFile):
     model = whisper.load_model("tiny")
-
-    # load audio and pad/trim it to fit 30 seconds
-    audio = whisper.load_audio(audioFile)
-    audio = whisper.pad_or_trim(audio)
-
-    # make log-Mel spectrogram and move to the same device as the model
-    mel = whisper.log_mel_spectrogram(audio).to(model.device)
-
-    # detect the spoken language
-    _, probs = model.detect_language(mel)
-    print(f"Detected language: {max(probs, key=probs.get)}")
-
-    # decode the audio
-    options = whisper.DecodingOptions(fp16 = False)
-    result = whisper.decode(model, mel, options)
+    result = model.transcribe(audioFile)
 
     # print the recognized text
-    print(result.text)
+    print(result["text"])
 
     with open(outputFile, 'w') as f:
-        f.write(result.text)
+        f.write(result["text"])
